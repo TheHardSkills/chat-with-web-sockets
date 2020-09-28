451390024269f1063f84d97b3f3422cea15d24ef
Yuliia Ok <yuliia.work.acc@gmail.com>
2020-09-28 11:19:32 +0300

    Adding logic for creating a new user

--- BusinessLogic/LoginController.js
+++ BusinessLogic/LoginController.js
@@ -2,14 +2,32 @@ const DataProvider = require("../DataProvider/DataProvider");
 const dataProvider = new DataProvider();
 
 class LoginController {
-    async tryLoginUser(username, password) {
-        let resultObject = { isLogin: "", error: "" };
+  async tryLoginUser(username, password) {
+    let loginUserInfo = await this.userCreation(username, password);
+    console.log("loginUserInfo", loginUserInfo);
+  }
+  async userCreation(username, password) {
+    let resultObject = { isLogin: false, error: "" };
 
-        if (!await dataProvider.findSomeOne()) dataProvider.createOneUser(username, password, true);
-        else dataProvider.createOneUser(username);
-        //return resultObject
+    let res = await dataProvider.findOneUserByFilter({ username });
+    if (res) {
+      if (password === res.password) {
+        //вход в чат
+        resultObject.isLogin = true;
+        return resultObject;
+      } //ошибка - не совпадает пароль
+      resultObject.error = "Incorrect password";
+      return resultObject;
     }
-    // checkUserExistence
+    //создвние нового юзера
+    if (!(await dataProvider.findSomeOne())) {
+      dataProvider.createOneUser(username, password, true);
+    } else {
+      dataProvider.createOneUser(username, password, false);
+    }
+    resultObject.isLogin = true;
+    return resultObject;
+  }
 }
 
 module.exports = LoginController;
--- DataProvider/DataProvider.js
+++ DataProvider/DataProvider.js
@@ -2,24 +2,18 @@ const mongoose = require("mongoose");
 const User = require("./Models/userModel");
 
 class DataProvider {
-    createOneUser(username, password, adminStatus) {
-        let user = new User({ username, password, adminStatus });
-        user.save(function (err, succ) {
-            if (err) return console.error(err);
-            else {
-                console.log("*Save successfully*");
-                return succ;
-            }
-        });
-    }
+  createOneUser(username, password, adminStatus) {
+    let user = new User({ username, password, adminStatus });
+    return user.save();
+  }
 
-    findSomeOne() {
-        return User.findOne().exec();
-    }
+  findSomeOne() {
+    return User.findOne().exec();
+  }
 
-    findOneUserByFilter(filter) {
-        return User.findOne(filter).exec();
-    }
+  findOneUserByFilter(filter) {
+    return User.findOne(filter).exec();
+  }
 }
 
 module.exports = DataProvider;
